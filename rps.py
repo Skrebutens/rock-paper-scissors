55d8555ab2868bd7ad85a6c09c8d7f8c0f5a7996
Skrebutens <149562137+Skrebutens@users.noreply.github.com>
2024-01-10 10:16:13 +0200

    Update rps.py

--- rps.py
+++ rps.py
@@ -11,4 +11,39 @@ SCISSOR = "2"
 PAPER = "3"
 
 print("Welcome to Rock, Paper, Scissors!")
+print("Its your turn to make your move")
+Move = input("Make your choice:")
+print("You chose: " + Move)
 
+if Move == ROCK:
+    print("Akmens")
+if Move == SCISSOR:
+    print("Šķēres")
+if Move == PAPER:
+    print("Papīrs")
+
+import random
+result = str(random.randint(1, 3))
+print("Computer chose:" + result)
+
+if result == ROCK:
+    print("Akmens")
+if result == SCISSOR:
+    print("Šķēres")
+if result == PAPER:
+    print("Papīrs")
+
+if Move == result:
+    print("DRAW")
+elif Move == ROCK and result == SCISSOR:
+    print("Computer chose SCISSORS, you WON!")
+elif Move == SCISSOR and result == PAPER:
+    print("Computer chose PAPER, you WON!")
+elif Move == PAPER and result == ROCK:
+    print("Computer chose ROCK, you WON!")
+elif Move == PAPER and result == SCISSOR:
+    print("Computer chose SCISSORS, you LOSE!")
+elif Move == SCISSOR and result == ROCK:
+    print("Computer chose ROCK, you LOSE!")
+elif Move == ROCK and result == PAPER:
+    print("Computer chose PAPER, you LOSE!")
